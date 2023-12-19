e964ea72d916b6861ca7b2db99f7f512d8681ade
Martin Pozniak <mpozniak@mmm.com>
2023-12-19 11:07:26 -0500

    Lifting gameboard state up out of gameboard component and into App.jsx in order to allow for us to derive who the winner of the game is. Adding a seperate file containing all winning combinations to check against to find a winner. Adding logic to determine a winner.

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,22 @@ import Player from "./components/Player/Player"
 import GameBoard from "./components/GameBoard/GameBoard"
 import Log from "./components/Log/Log";
 
+import { WINNING_COMBINATIONS } from "./assets/winning-combinations";
+
 import { useState } from "react"
 
+/***********************************************************
+* GLOBAL CONSTANTS
+/***********************************************************/
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null]
+]
+
+/***********************************************************
+* GLOBAL FUNCTION: deriveActivePlayer
+/***********************************************************/
 function deriveActivePlayer(gameTurns) {
 
   let currentPlayer = 'X';
@@ -17,13 +31,56 @@ function deriveActivePlayer(gameTurns) {
 
 }
 
+/***********************************************************
+* Function: App
+/***********************************************************/
 function App() {
 
+  /***********************************************************
+  * Member Variables
+  /***********************************************************/
+  // State Variables
   const [gameTurns, setGameTurns] = useState([]);
 
+  // Derived State Members
   const activePlayer = deriveActivePlayer(gameTurns);
 
+  let winner = undefined;
+  let gameBoard = initialGameBoard;
+
+  // Derive the gameboard based on turns
+  for (const turn of gameTurns) {
+
+    // Destructure a turn
+    const { square, player } = turn;
+
+    // Destructure row and col from a square
+    const { row, col } = square;
+
+    gameBoard[row][col] = player;
+
+  }
+
+  // Derive if there is a winner
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column]
+    const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column]
+    const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column]
+
+    if (firstSquareSymbol && firstSquareSymbol === secondSquareSymbol && secondSquareSymbol === thirdSquareSymbol) {
+      // We have a winner
+      winner = firstSquareSymbol;
+    }
+
+
+  }
+
+  /***********************************************************
+  * Function: handleSelectSquare
+  /***********************************************************/
   function handleSelectSquare(rowIndex, colIndex) {
+
+    // This function must be inside the app component because it leverages a state variable.
 
     setGameTurns((prevTurns) => {
 
@@ -43,6 +100,9 @@ function App() {
 
   }
 
+  /***********************************************************
+  * Function: MAIN JSX
+  /***********************************************************/
   return (
     <>
       <main>
@@ -55,7 +115,8 @@ function App() {
           </ol>
 
           {/* Game Board Section */}
-          <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} activePlayerSymbol={activePlayer}></GameBoard>
+          {winner ? <h1>You win {winner}!</h1> : undefined}
+          <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} activePlayerSymbol={activePlayer} board={gameBoard}></GameBoard>
 
 
         </div>
@@ -66,7 +127,6 @@ function App() {
       </main>
     </>
   )
-
 
 }
 
--- src/components/GameBoard/GameBoard.jsx
+++ src/components/GameBoard/GameBoard.jsx
@@ -1,51 +1,9 @@
-import { useState } from "react"
-
-const initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null]
-]
-
-export default function GameBoard({ onSelectSquare, turns }) {
-
-  let gameBoard = initialGameBoard;
-
-  for (const turn of turns) {
-
-    // Destructure a turn
-    const { square, player } = turn;
-
-    // Destructure row and col from a square
-    const { row, col } = square;
-
-    gameBoard[row][col] = player;
-
-  }
-
-  // Code from previous example where gameboard state was managed internally here.
-  // const [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-  // function handleSelectSquare(selectedRow, selectedCol) {
-
-  //   setGameBoard((previousGameBoard) => {
-
-  //     // Required to create a full copy of the previous game board so we aren't updating the state in a muteable way but instead in an immutable way
-  //     const updatedGameBoard = [...previousGameBoard.map(innerArray => [...innerArray])];
-
-  //     updatedGameBoard[selectedRow][selectedCol] = activePlayerSymbol;
-
-  //     return updatedGameBoard;
-
-  //   });
-
-  //   onSelectSquare();
-
-  // }
+export default function GameBoard({ onSelectSquare, board }) {
 
   return (
     <>
       <ol id="game-board">
-        {gameBoard.map((row, rowIndex) =>
+        {board.map((row, rowIndex) =>
           <li key={rowIndex}>
             <ol>
               {row.map((playerSymbol, colIndex) =>
